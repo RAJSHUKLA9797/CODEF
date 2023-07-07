2d33c0bdf7f09d26c61e633ddf8835c2aa3ee69c
RAJSHUKLA9797 <101661663+RAJSHUKLA9797@users.noreply.github.com>
2023-07-07 17:54:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/',(req,res)=>{
   res.send('heeello')
 })
+
+
+/**get user root */
 app.get('/user',(req,res)=>{
     res.render('form.ejs')
 })
@@ -22,12 +25,52 @@ app.post('/user',async(req,res)=>{
       // const userInfo = response.data.result[0];
       
       var udata = response.data.result[0]
-      console.log(udata)
+      // console.log(udata)
       // res.json(userInfo);
       res.render('display.ejs', {udata})
     } catch (error) {
       res.status(500).json({ error: 'Failed to fetch user information' });
     }
+})
+/*yha user ke problems ka route bna */
+app.get('/user/problems',(req,res)=>{
+  res.render('problems.ejs')
+})
+/**yha pr dropdown se post request accept kro */
+app.post('/:user/topic',async(req,res)=>{
+  let solvedProblems = []
+  const{tag, username}=req.body;
+  // console.log(tag);
+  // console.log(username);
+  try{
+    const response = await axios.get(`https://codeforces.com/api/user.status?handle=${username}&from=1&count=100000`)
+    var allproblems = response.data.result
+    for(let prob of allproblems){
+        if(prob.verdict=='OK')
+        {
+          for (let tagSearched of prob.problem.tags)
+          {
+            if (tagSearched == tag)
+            {
+              solvedProblems.push({
+                name : prob.problem.name,
+                contestId : prob.problem.contestId,
+                index : prob.problem.index,
+                rating : prob.problem.rating
+              })
+            }
+          }
+        }
+    }
+    solvedProblems.sort((a, b) => {
+      return a.rating - b.rating;
+    });
+    console.log(allproblems)
+    res.render('problems.ejs',{solvedProblems})
+  }
+  catch(error){
+    res.status(500).json({error:'failed'})
+  }
 })
 app.listen(3000,()=>{
     console.log("on port 3000!!!")
